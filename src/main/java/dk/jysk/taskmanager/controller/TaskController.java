93dc4b88a4c276333f52295803fec4f5a577f781
Izak90 <c.as.i@hotmail.com>
2025-11-08 15:03:12 +0000

    create controller with annotations @valid, for request validation and @ResponseStatus for correct http request responses.

---
+++ src/main/java/dk/jysk/taskmanager/controller/TaskController.java
@@ -0,0 +1,45 @@
+package dk.jysk.taskmanager.controller;
+
+import dk.jysk.taskmanager.entity.TaskEntity;
+import dk.jysk.taskmanager.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService service;
+
+    @GetMapping
+    public List<TaskEntity> getAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TaskEntity getById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskEntity create(@Valid @RequestBody TaskEntity taskEntity) {
+        return service.save(taskEntity);
+    }
+
+    @PutMapping("/{id}")
+    public TaskEntity update(@PathVariable Long id, @Valid @RequestBody TaskEntity taskEntity) {
+        return service.update(id, taskEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
